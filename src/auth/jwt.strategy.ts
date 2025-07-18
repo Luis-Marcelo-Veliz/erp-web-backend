db7ea0bf7c717ea3fed1e27b7804d93c96fe8a52
lvelo <lveliz60@gmail.com>
2025-07-18 11:27:11 -0300

    Create jwt.strategy.ts

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'SECRET_KEY',
+    });
+  }
+
+  async validate(payload: any) {
+    // payload contiene la información que firmaste (username, sub, role)
+    return { userId: payload.sub, username: payload.username, role: payload.role };
+  }
+}
