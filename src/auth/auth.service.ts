1e71707d5e858d6677391b183bd415475bd3a8f2
lvelo <lveliz60@gmail.com>
2025-07-22 21:04:03 +0000

    commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,11 +5,15 @@ import { JwtService } from '@nestjs/jwt';
 export class AuthService {
   constructor(private jwtService: JwtService) {}
 
-  async login(username: string, password: string) {
-    // Aquí validas con base de datos o lista fija:
-    if (username !== 'admin' || password !== '1234')
-      throw new UnauthorizedException('Credenciales inválidas');
+   async login(username: string, password: string) {
+    console.log('AuthService.login recibidos:', { username, password });
+    if (username !== 'admin' || password !== '1234') {
+      console.log('Credenciales inválidas');
+      throw new UnauthorizedException();
+    }
     const payload = { username, sub: 1, role: 'admin' };
-    return { accessToken: this.jwtService.sign(payload) };
+    const token = this.jwtService.sign(payload);
+    console.log('Emitiendo token:', token);
+    return { accessToken: token };
   }
 }
