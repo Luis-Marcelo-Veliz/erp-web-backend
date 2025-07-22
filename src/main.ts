9ea3ec125e2ea93b7a8f245713c64de85c8fa6d3
lvelo <lveliz60@gmail.com>
2025-07-22 17:23:56 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // Habilita CORS para tu frontend (o '*' en dev)
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(3000);
   console.log('🚀 Application is running on: http://localhost:3000');
 }
