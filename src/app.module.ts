6228ea7f9658cc111d7eeb4ec42436bc5341d5ee
lvelo <lveliz60@gmail.com>
2025-07-28 14:30:16 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,25 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { Product } from './products/product.entity';
 import { AppController } from './app.controller';
 
 @Module({
-imports: \[
-ConfigModule.forRoot({ isGlobal: true }),
-AuthModule,
-TypeOrmModule.forRoot({
-type: 'sqlite',
-database: 'db.sqlite',
-entities: \[\_\_dirname + '/\*\*/\*.entity{.ts,.js}'],
-synchronize: true,
-}),
-ProductsModule,
-],
-controllers: \[AppController],
-providers: \[],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Product],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+  controllers: [AppController],
+  providers: [],
 })
-export class AppModule {} 
+export class AppModule {}
